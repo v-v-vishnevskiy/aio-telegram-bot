b80d2dda5c47c4b9fae0c59eace95553c5abd991
Valery Vishnevskiy <v.v.vishnevskiy@gmail.com>
2019-04-29 22:45:35 +0300

    Refactoring bot.py

--- telegrambot/bot.py
+++ telegrambot/bot.py
@@ -25,33 +25,33 @@ class Bot:
         self.client = client
         self._handlers: Handlers = handlers or Handlers()
         self._context = context
-        self._scheduler: Optional[aiojobs.Scheduler] = None
-        self._closed = True
-        self._update_id = 0
+        self.__scheduler: Optional[aiojobs.Scheduler] = None
+        self.__closed = True
+        self.__update_id = 0
         self.__chat_id = contextvars.ContextVar("chat_id")
 
-    async def initialize(self, *, webhooks: bool = False, **kwargs):
-        if self._closed is False:
+    async def initialize(self, *, webhooks: bool = False, interval: float = 0.1, **kwargs):
+        if self.__closed is False:
             return
 
         if len(self._handlers) == 0:
             raise BotError("Can't initialize with no one handler")
 
-        self._closed = False
-        self._scheduler = await aiojobs.create_scheduler(**kwargs)
+        self.__closed = False
+        self.__scheduler = await aiojobs.create_scheduler(**kwargs)
         if webhooks is False:
-            await self._scheduler.spawn(self._get_updates())
+            await self.__scheduler.spawn(self._get_updates(interval))
 
     async def close(self):
-        if self._closed:
+        if self.__closed:
             return
 
-        self._closed = True
+        self.__closed = True
 
-        await self._scheduler.close()
-        self._scheduler = None
+        await self.__scheduler.close()
+        self.__scheduler = None
 
-        self._update_id = 0
+        self.__update_id = 0
 
     def add_handler(self, handler: Callable, *args, **kwargs):
         self._handlers.add(*args, **kwargs)(handler)
@@ -61,30 +61,30 @@ class Bot:
         await self.client.request("get", "sendMessage", params={"chat_id": chat_id, "text": text})
 
     async def process_update(self, data: dict):
-        if self._closed is True:
+        if self.__closed is True:
             raise RuntimeError("The bot isn't initialize")
 
         incoming, message_type = _recognize_type(data)
         handler = self._handlers.get(incoming, message_type, data)
         if handler:
             # TODO: не спаунить, если хэндлер не нужно запускать
-            await self._scheduler.spawn(
+            await self.__scheduler.spawn(
                 self.__handler(handler, Message(self, data, self._context, incoming, message_type))
             )
 
-    async def _get_updates(self):
-        while self._closed is False:
-            params = {"offset": self._update_id} if self._update_id else {}
+    async def _get_updates(self, interval: float):
+        while self.__closed is False:
+            params = {"offset": self.__update_id} if self.__update_id else {}
             data = await self.client.request("get", "getUpdates", params=params)
             await self._process_updates(data)
-            await asyncio.sleep(0.1)
+            await asyncio.sleep(interval)
 
     async def _process_updates(self, data: Union[None, dict]):
         if data:
             for raw in data["result"]:
                 await self.process_update(raw)
-                self._update_id = max(raw["update_id"], self._update_id)
-            self._update_id += 1 if data["result"] else 0
+                self.__update_id = max(raw["update_id"], self.__update_id)
+            self.__update_id += 1 if data["result"] else 0
 
     async def __handler(self, handler: Handler, message: Message):
         if message.incoming is not None and message.incoming.is_message_or_post:
