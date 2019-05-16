9efa4b78caec45a7856a356d4fca8bdb8bbb69b0
Valery Vishnevskiy <v.v.vishnevskiy@gmail.com>
2019-05-16 23:56:29 +0300

    Add TelegramApiError exception

--- aiotelegrambot/errors.py
+++ aiotelegrambot/errors.py
@@ -1,5 +1,18 @@
+from typing import Optional
+
+from aiohttp.client import ClientResponse
+
+
 class BotError(Exception):
     pass
+
+
+class TelegramApiError(BotError):
+
+    def __init__(self, msg: str, data: Optional[dict], response: ClientResponse):
+        self.data = data
+        self.response = response
+        super().__init__(msg)
 
 
 class HandlerError(BotError):
