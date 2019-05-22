d4456255b0d3dfcb51fad6a2cb991fb279889a98
Valery Vishnevskiy <v.v.vishnevskiy@gmail.com>
2019-05-23 01:23:58 +0300

    Set version to 1.0.0

--- aiotelegrambot/__init__.py
+++ aiotelegrambot/__init__.py
@@ -5,4 +5,4 @@ from aiotelegrambot.message import Message
 from aiotelegrambot.types import Chat, Content, Incoming
 
 __all__ = ["Bot", "Chat", "Client", "Content", "Handler", "Handlers", "Incoming", "Message"]
-__version__ = "1.0.0b"
+__version__ = "1.0.0"
